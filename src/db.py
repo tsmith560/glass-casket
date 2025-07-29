821ac67d9ca4b4496a8df1c25ad980a6c6d9ec87
Tiffany Smith <thirstsnake@MacBookPro.lan>
2025-07-29 19:39:04 -0400

    Losing my mind

--- src/db.py
+++ src/db.py
@@ -1,15 +1,15 @@
-# src/db.py
-import pg8000
+import psycopg2
 from dotenv import load_dotenv
 import os
 
-load_dotenv()  # Load .env into environment variables
+load_dotenv()
 
 def get_connection():
-    return pg8000.connect(
+    return psycopg2.connect(
         dbname=os.getenv("DB_NAME"),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
         host=os.getenv("DB_HOST"),
         port=os.getenv("DB_PORT")
     )
+
